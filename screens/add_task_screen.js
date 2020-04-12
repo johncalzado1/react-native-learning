c248d96fe37b6c257fcb014b434f7a9357b7ca86
toptechtips <toptechtips497@gmail.com>
2020-04-12 21:16:15 +0100

    added sub task selection for tasks

--- screens/add_task_screen.js
+++ screens/add_task_screen.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Text, Portal, Divider, Title, Button, Caption, TextInput, IconButton } from 'react-native-paper'
-import { View, ScrollView, StyleSheet, Platform } from 'react-native';
+import { Portal, Title, Button, Caption, Dialog, Text, Subheading } from 'react-native-paper'
+import { View, ScrollView, StyleSheet } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
-import moment from 'moment';
 import { TimeInputControl } from '../theme'
 import { theme_file } from '../theme_file';
 
@@ -20,33 +19,75 @@ const styles = StyleSheet.create({
 
 export const AddTaskScreen = () => {
     return (
-        <Portal.Host>
-            <ScrollView style={{ margin: 10 }}>
-                <CategorySection />
-                <TimeSection />
-            </ScrollView>
-        </Portal.Host>
+        <ScrollView style={{ margin: 10 }}>
+            <CategorySection />
+            <TimeSection />
+        </ScrollView>
     )
 }
 
 const CategorySection = () => {
+    const [task, toggleTask] = useState(undefined);
+    const [subTask, toggleSubTask] = useState(undefined);
     const [taskTypes, setTaskTypes] = useState([])
     const [loading, setLoading] = useState(true);
+
+    const [showSubTaskModal, toggleShowSubTaskModal] = useState(false)
     const ref = firestore().collection('task_type');
+
+    const toggleButtonTask = (taskName) => {
+
+        // if the task is already selected, then toggle it off
+        if (task === taskName) {
+            toggleTask('');
+            toggleSubTask('')
+        } else {
+            toggleTask(taskName)
+            toggleSubTask('')
+            toggleShowSubTaskModal(true)
+        }
+    }
+
+    const toggleButtonSubTask = (taskName) => {
+
+        // if the task is already selected, then toggle it off
+        if (subTask === taskName) {
+            toggleSubTask('');
+        } else {
+            toggleSubTask(taskName)
+            toggleShowSubTaskModal(false)
+        }
+    }
 
     useEffect(() => {
         return ref.onSnapshot((querySnapshot) => {
-            const list = [];
+            const taskTypesList = [];
             querySnapshot.forEach(doc => {
                 const { name, title } = doc.data();
-                list.push({
+                const taskObj = {
                     id: doc.id,
                     title,
                     name,
+                    subtasks: {}
+                };
+
+                ref.doc(doc.id).collection('subtask_type').onSnapshot(subTaskQuerySnapshot => {
+                    const subTaskTypes = [];
+                    subTaskQuerySnapshot.forEach(subtask_doc => {
+
+                        const { name, title } = subtask_doc.data();
+                        subTaskTypes.push({
+                            id: subtask_doc.id,
+                            name,
+                            title
+                        });
+                    });
+                    taskObj['subtasks'] = subTaskTypes
                 });
+                taskTypesList.push(taskObj)
             });
 
-            setTaskTypes(list);
+            setTaskTypes(taskTypesList);
 
             if (loading) {
                 setLoading(false);
@@ -59,15 +100,50 @@ const CategorySection = () => {
             return (
                 <Button
                     key={i}
-                    mode="outlined"
+                    mode={(task === option.name) ? "contained" : "outlined"}
                     compact
-                    color={theme_file.colors.text}
+                    color={(task === option.name) ? theme_file.colors.primary : theme_file.colors.text}
                     uppercase={false}
-                    style={styles.button}>
+                    style={styles.button}
+                    onPress={() => toggleButtonTask(option.name)}>
                     {option.title}
                 </Button>
             )
         })
+    }
+
+    const renderSubOptions = () => {
+        if (!task || task === '') return (<></>)
+        const taskObj = taskTypes.find(taskObj => taskObj.name === task)
+        console.log(taskObj)
+        if ('subtasks' in taskObj) {
+            if (taskObj['subtasks'].length < 1) return (<Text>No Sub Tasks</Text>)
+            const subTasks = taskObj['subtasks'];
+            return subTasks.map((option, i) => {
+                return (
+                    <Button
+                        key={i}
+                        mode={(subTask === option.name) ? "contained" : "outlined"}
+                        compact
+                        color={(subTask === option.name) ? theme_file.colors.primary : theme_file.colors.text}
+                        uppercase={false}
+                        style={styles.button}
+                        onPress={() => toggleButtonSubTask(option.name)}>
+                        {option.title}
+                    </Button>
+                )
+            })
+        } else {
+            return (
+                <Text>No Sub Tasks</Text>
+            )
+        }
+    }
+
+    const getSubTaskTitle = () => {
+        const taskObj = taskTypes.find(taskObj => taskObj.name === task)
+        const subTaskObj = taskObj['subtasks'].find(subTaskObj => subTaskObj.name === subTask)
+        return subTaskObj.title
     }
     return (
         <View>
@@ -75,6 +151,17 @@ const CategorySection = () => {
             <View style={styles.buttonContainer}>
                 {renderOptions()}
             </View>
+            {(subTask !== '' && subTask !== undefined) && (
+                <Subheading>Sub Task: {getSubTaskTitle()}</Subheading>
+            )}
+            <Portal>
+                <Dialog visible={showSubTaskModal} onDismiss={() => toggleShowSubTaskModal(false)}>
+                    <Dialog.Title>Choose a sub task</Dialog.Title>
+                    <Dialog.Content style={styles.buttonContainer}>
+                        {renderSubOptions()}
+                    </Dialog.Content>
+                </Dialog>
+            </Portal>
         </View >
     )
 }
